42b6af00f8b4ad92135625a3bbd2c82288f5a3e1
Brianne <e.brianne.matthews@gmail.com>
2019-09-03 21:47:26 -0700

    Cleaning

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,7 @@ import React from "react";
 import * as firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyCaxt87Q-q7sKHvPQ_7Dy7twykBm7h-eoU",
-  authDomain: "galleryloaf.firebaseapp.com",
-  databaseURL: "https://galleryloaf.firebaseio.com",
-  projectId: "galleryloaf",
-  storageBucket: "galleryloaf.appspot.com",
-  messagingSenderId: "3295628774",
-  appId: "1:3295628774:web:38dbe0d6ec682942"};
+  };
 firebase.initializeApp(config);
 var db = firebase.firestore();
 export default db;
